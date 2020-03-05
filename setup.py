85223dd74ca3b2b02d159469e556c6e6aeec29fc
Vadym Hutei <hutei@live.com>
2020-03-05 08:41:17 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='ucore',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Ukubuka Core',
+    author='Vadym Hutei',
+    author_email='hutei@live.com'
+)
