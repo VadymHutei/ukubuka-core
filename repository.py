4d7d105f2256a8bd8fefa642c6e3a21d5f738bb4
Vadym Hutei <hutei@live.com>
2021-04-13 23:05:11 +0300

    repository

--- repository.py
+++ repository.py
@@ -3,8 +3,22 @@ import pymysql
 
 class Repository:
 
+    DBCredentialKeys = (
+        'host',
+        'user',
+        'password',
+        'database',
+    )
+
     def __init__(self):
-        self.dbCredentials = {}
+        self.dbCredentials = {
+            'cursorclass': pymysql.cursors.DictCursor,
+        }
 
     def getConnection(self):
         return pymysql.connect(**self.dbCredentials)
+
+    def _setCredentials(self, credentials):
+        for key, value in credentials.items():
+            if key in self.DBCredentialKeys:
+                self.dbCredentials[key] = value
