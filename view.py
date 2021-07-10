bb3fe11edea4e4b170c6309c1a6903a3149c43f5
Vadym Hutei <hutei@live.com>
2021-07-10 19:41:13 +0300

    view

--- view.py
+++ view.py
@@ -3,25 +3,25 @@ from flask import render_template
 
 class View:
 
-    def __init__(self, template=None, language=None):
-        self.data = {
-            't': {}
-        }
+    def __init__(self, template=None):
         self.template = template
+        self.data = {}
+        self.templateData = {}
 
     def addData(self, data):
         self.data.update(data)
 
-    def render(self):
-        self.prepageData()
-        return render_template(self.template, **self.data)
-
     def prepageData(self):
         self.prepareTemplateData()
         self.preparePageData()
+        self.data['t'] = self.templateData
 
     def prepareTemplateData(self):
         pass
 
     def preparePageData(self):
         pass
+
+    def render(self):
+        self.prepageData()
+        return render_template(self.template, **self.data)
